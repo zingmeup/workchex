c8231f82cc39907f97193c806078fe2f369439ba
Chetan Gulati <chetan.gulati75@gmail.com>
2020-01-19 17:49:25 +0530

    logo height

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,16 @@ $(function () {
 
     //===== Sticky
 
-    // $(window).on('scroll', function (event) {
-    //     var scroll = $(window).scrollTop();
-    //     if (scroll < 20) {
-    //         $(".navbar-area").removeClass("sticky");
-    //         $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
-    //     } else {
-    //         $(".navbar-area").addClass("sticky");
-    //         $(".navbar .navbar-brand img").attr("src", "assets/images/logo-2.svg");
-    //     }
-    // });
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".navbar-area").removeClass("sticky");
+            // $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
+        } else {
+            $(".navbar-area").addClass("sticky");
+            // $(".navbar .navbar-brand img").attr("src", "assets/images/logo-2.svg");
+        }
+    });
 
 
 
